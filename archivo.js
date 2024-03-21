efca8e05a4364f324c3649833790cbfd5d5b1f95
Cristian Robles <cristian210206.jcrd@gmail.com>
2024-03-21 10:54:45 +0100

    Este es un cambio para poderlo subir al repo

--- archivo.js
+++ archivo.js
@@ -1,1 +1,1 @@
-console.log("Cambio");
+console.log("Este es un cambio realizado para que se pueda subir al repo, el repo usado en el laboratorio es https://github.com/Cristian210206/Repo-local.git");
